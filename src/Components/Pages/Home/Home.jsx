29351b16bda72db9b6e242f24449b347c7f5dc79
tamzid620 <tamzid620@gmail.com>
2024-10-01 12:11:11 +0600

    home Count up component  added

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 import Banner from "../../Shared/HomeSection/Banner/Banner";
+import HomeCountUp from "../../Shared/HomeSection/HomeCountUp/HomeCountUp";
 import Section0 from "../../Shared/HomeSection/Section-0/Section0";
 import Section1 from "../../Shared/HomeSection/Section-1/Section1";
 import Section2 from "../../Shared/HomeSection/Section-2/Section2";
 import EveryPart from "../../Shared/HomeSection/Section-3/EveryPart";
-// import Section3 from "../../Shared/HomeSection/Section-3/Section3";
 import Section4 from "../../Shared/HomeSection/Section-4/Section4";
-// import Section5 from "../../Shared/HomeSection/Section-5/Section5";
 import Section6 from "../../Shared/HomeSection/Section-6/Section6";
 import TestingQA from "../../Shared/Navbar/TestingQA/TestingQA";
 
@@ -14,6 +13,8 @@ const Home = () => {
     <>
       <Banner />
      
+      {/*----------------- Home CountUp Section  -----------------*/}
+      <HomeCountUp/>
       {/*----------------- Explore Our Popular Service -----------------*/}
       <Section0 />
       {/* ---------------Explore Our Offering -------------*/}
---
+++ src/Components/Shared/HomeSection/HomeCountUp/HomeCountUp.jsx
@@ -0,0 +1,39 @@
+import CountUp from "react-countup";
+
+const HomeCountUp = () => {
+  return (
+    <div
+      style={{ fontFamily: "Open Sans, sans-serif" }}
+      className="py-[80px] lg:max-w-6xl md:max-w-3xl sm: max-w-sm lg:mx-auto md:mx-7 sm: mx-4"
+    >
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 sm: grid-cols-1 gap-5">
+        <div className="lg:border-r-2 border-purple-300">
+            <span className=" text-4xl text-purple-900">
+                <span><CountUp start={0} end={4} duration={2}/></span><span>+</span>
+            </span>
+          <h1 className="font-semibold text-lg">Years of Expertise</h1>
+        </div>
+        <div className="lg:border-r-2 border-purple-300">
+            <span className="text-4xl text-purple-900">
+                <span><CountUp start={0} end={20} duration={2}/></span><span>+</span>
+            </span>
+          <h1 className="font-semibold text-lg">IT Professionals</h1>
+        </div>
+        <div className="lg:border-r-2 border-purple-300">
+            <span className="text-4xl text-purple-900">
+                <span><CountUp start={0} end={230} duration={2}/></span><span>+</span>
+            </span>
+          <h1 className="font-semibold text-lg">Success Stories</h1>
+        </div>
+        <div>
+            <span className="text-4xl text-purple-900">
+                <span><CountUp start={0} end={15} duration={2}/></span><span>+</span>
+            </span>
+          <h1 className="font-semibold text-lg">Industries Covered</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCountUp;
