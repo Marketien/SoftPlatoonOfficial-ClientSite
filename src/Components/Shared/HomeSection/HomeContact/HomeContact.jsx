7a32d74e6eec85916a2679d97c4eb0c6d973714d
tamzid620 <tamzid620@gmail.com>
2024-10-10 14:09:35 +0600

    homeContact email js error fixed

--- src/Components/Shared/HomeSection/HomeContact/HomeContact.jsx
+++ src/Components/Shared/HomeSection/HomeContact/HomeContact.jsx
@@ -1,55 +1,56 @@
-import './HomeContact.css';
+import "./HomeContact.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { useEffect, useRef, } from "react";
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
-import emailjs from '@emailjs/browser';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useRef, useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loading from "../../Loading/Loading";
 
 const HomeContact = () => {
-  const fileInputRef = useRef(null);
-  const handleClick = () => {
-    fileInputRef.current.click();
-  };
-  useEffect(() => {
-    Aos.init({ duration: 1000 }); 
-  }, []);
-
+  const [isLoading, setLoading] = useState(false);
+  const [contact, setContact] = useState("");
   const form = useRef();
+
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
 
   const sendEmail = (e) => {
     e.preventDefault();
+    setLoading(true);
+
 
     emailjs
-      .sendForm('service_ml4shge', 'template_0pd37zu', form.current, {
-        publicKey: 'Uktdumaxl2EPWGl4T',
+      .sendForm("service_nu370bm", "template_l0d9yqb", form.current, {
+        publicKey: "QCIFFc_9aZQkMdLJj",
       })
       .then(
         () => {
           toast.success("Email Sent Successfully!", {
-             position: "top-right"
+            position: "top-right",
           });
+          setLoading(false);
         },
         (error) => {
-          toast.error("Something Went Wrong !", {
-             position: "top-right"
+          toast.error("Error: " + error.text, {
+            position: "top-right",
           });
+          setLoading(false);
           console.log(error);
-        },
+        }
       );
   };
-  
+
   return (
-    <div id='contact' className="bg-gray-100 py-[180px] ">
+    <div id="contact" className="bg-gray-100 py-[180px] ">
       <div
         style={{ fontFamily: "Open Sans, sans-serif" }}
-        className=" lg:max-w-6xl md:max-w-3xl sm: max-w-sm lg:mx-auto md:px-7 sm: px-4"
+        className=" lg:max-w-6xl md:max-w-3xl sm:max-w-sm lg:mx-auto md:px-7 sm:px-4"
       >
-        {/* email input section  */}
         <div data-aos="fade-up" style={{ fontFamily: "Ubuntu,serif" }}>
-          {/* form div  */}
           <form ref={form} onSubmit={sendEmail} className="bg-white px-5 py-5">
             <h1 className="text-xl mb-3">Need A Consultation? </h1>
             <p className="mb-4">
@@ -62,30 +63,13 @@ const HomeContact = () => {
               id="feedback"
               placeholder="How can I help you? "
             />
-            {/* <p>Browse <input className=' text-xs ' type="file" id="fileInput" /> to upload</p>  */}
-            <div className="flex gap-2 mb-4">
+            <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-3">
               <input
-                name='file'
-                id="file"
-                type="file"
-                ref={fileInputRef}
-                style={{ display: "none" }}
-              />
-              <p
-                onClick={handleClick}
-                style={{ cursor: "pointer", color: "rgb(31, 145, 211)" }}
-              >
-                Browse
-              </p>
-              to upload your file(s)
-            </div>
-            <div className="grid lg:grid-cols-2 md:grid-cols-2 sm: grid-cols-1 gap-3">
-              <input
-              required
+                required
                 className="border border-gray-300 px-3 py-2 w-full"
-                type="text"
                 name="name"
                 id="name"
+                type="text"
                 placeholder="Full Name"
               />
               <input
@@ -93,28 +77,51 @@ const HomeContact = () => {
                 type="text"
                 name="company"
                 id="company"
-                placeholder="Company"
+                placeholder="Company Name"
               />
               <input
-              required
+                required
                 className="border border-gray-300 px-3 py-2 w-full"
                 type="email"
                 name="email"
                 id="email"
                 placeholder="Email"
               />
+              <input
+              hidden
+                required
+                className="border border-gray-300 px-3 py-2 w-full"
+                type="number"
+                name="phone"
+                id="phone"
+                value={contact}
+                placeholder="Phone No"
+              />
               <PhoneInput
-                country={"us"} 
-                name="phoneNo"
-                id="phoneNo"
+                country={"us"}
+                name="contact"
+                id="contact"
+                type="number"
+                value={contact} // Set the value from the state
+                onChange={setContact} 
                 placeholder="Phone Number"
-                inputClass=" w-full"
+                inputClass="w-full"
               />
             </div>
-            <div className='flex justify-center'>
-              <button type='submit' className='my-8 px-8 py-3 uppercase hover:text-black hover:bg-gray-300 text-white bg-[#501b79] rounded-sm'>Send</button>
+            <div className="flex justify-center">
+              {isLoading ? (
+                <div className="my-8 px-8 py-3 bg-white rounded-sm">
+                  <Loading />
+                </div>
+              ) : (
+                <button
+                  type="submit"
+                  className="my-8 px-8 py-3 uppercase hover:text-black hover:bg-gray-300 text-white bg-[#501b79] rounded-sm"
+                >
+                  Send
+                </button>
+              )}
             </div>
-
           </form>
         </div>
       </div>
---
+++ src/Components/Shared/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+
+
+const Loading = () => {
+    return (
+        <span className="loading loading-spinner text-[#5A287F] w-[50px]"></span>
+    );
+};
+
+export default Loading;
